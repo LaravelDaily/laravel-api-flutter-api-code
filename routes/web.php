3f3db701382f7722bbc820a465774d8e17ecbec7
Modestas Vaitkevičius <info@modestasv.lt>
2025-03-20 13:08:24 +0200

    Installed Laravel API (Sanctum, Routes, Personal access tokens)

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::group(['prefix' => 'api'], function () {
-    Route::resource('categories', CategoryController::class);
 });
