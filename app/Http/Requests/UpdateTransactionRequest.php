4b5f2e4d37fae8252a669dbbef25e5e780e44f9d
Modestas Vaitkevičius <info@modestasv.lt>
2025-03-21 10:48:03 +0200

    Added transactions CRUD

---
+++ app/Http/Requests/UpdateTransactionRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTransactionRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'category_id' => ['required', 'integer'],
+            'user_id' => ['nullable', 'integer'],
+            'transaction_date' => ['required', 'date'],
+            'amount' => ['required', 'numeric'],
+            'description' => ['required'],
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
