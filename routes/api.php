2a1a4bd14fa9bbd2c471dbfbde4045685256efbd
Modestas Vaitkevičius <info@modestasv.lt>
2025-03-21 11:46:37 +0200

    Added API authentication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\TransactionsController;
 use Illuminate\Http\Request;
@@ -9,5 +10,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::resource('categories', CategoryController::class);
-Route::apiResource('transactions', TransactionsController::class);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('transactions', TransactionsController::class);
+});
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
