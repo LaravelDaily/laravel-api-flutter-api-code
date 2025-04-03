543199099911d269f167ec576f92e1233ee7d709
Modestas Vaitkevičius <info@modestasv.lt>
2025-04-03 13:12:50 +0300

    Updated Logout security

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
 
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
-Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware(['auth:sanctum']);
